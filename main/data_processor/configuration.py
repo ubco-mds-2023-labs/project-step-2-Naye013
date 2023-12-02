2e7a19f4ce95b8d91d3dc94b0a243ff17770b7a0
Nayeli Montiel <133909181+Naye013@users.noreply.github.com>
2023-12-01 21:48:29 -0800

    Documentation and updates Configuration.py
    I change the initialization from the attributes of the class from the method read_config instead of from the constructor init. Additionally, I add a validation for the path_file (e.g. .JSON, .json, .jSoN, etc.). Finally, I add the method get to properties from the config.json one by one.

--- main/data_processor/configuration.py
+++ main/data_processor/configuration.py
@@ -1,60 +1,94 @@
 import os
 import json
-import xml.etree.ElementTree as ET
-import csv
+import difflib
 
 class Config:
-    def __init__(self, path_file, data_type, entity_collection, base_fields, computable_fields):
-        self.path_file = path_file
-        self.data_type = data_type
-        self.entity_collection = entity_collection
-        self.base_fields = base_fields
-        self.computable_fields = computable_fields
+    def __init__(self):
+        """
+        Initializes a Config instance.
+
+        Attributes:
+            path (str): Absolute or relative path from the file in the configuration file.
+            data_type (str): The type of data in the configuration file (e.g., 'JSON', 'XML', 'CSV').
+            entity_collection (str): The collection of entities in the data.
+            base_field (str): The base field used in the configuration.
+            computable_fields (list): A list of computable fields in the configuration.
+            config_data (dict): The configuration data read from the file.
+        """
+        self.data_type = ''
+        self.entity_collection = ''
+        self.base_field = ''
+        self.computable_fields = []
+        self.path = os.path.join(os.getcwd(), 'config.json')
+        self.config_data = self.read_config()
 
     def is_valid_config(self):
+        """
+        Checks if the configuration is valid.
+
+        Returns:
+            bool: True if the configuration is valid, False otherwise.
+        """
         # Check if the file path exists
-        if not os.path.exists(self.path_file):
-            print(f"Error: File '{self.path_file}' does not exist.")
+        if not os.path.exists(self.path):
+            print(f"Error: File '{self.path}' does not exist.")
             return False
 
         # Extract the file extension
-        _, file_extension = os.path.splitext(self.path_file)
+        _, file_extension = os.path.splitext(self.path)
 
-        # Check if the file extension corresponds to a valid data type
-        valid_extensions = {'.json': 'JSON', '.xml': 'XML', '.csv': 'CSV'}
+        # Check if the file extension corresponds to a valid data type or a close match
+        valid_extensions = ['.json', '.xml', '.csv']
         if file_extension.lower() not in valid_extensions:
-            print(f"Error: Invalid file type for '{self.path_file}'. Supported types are JSON, XML, and CSV.")
-            return False
-
-        # Check if the specified data type matches the detected file extension
-        if self.data_type != valid_extensions[file_extension.lower()]:
-            print(f"Error: Data type '{self.data_type}' does not match the file extension for '{self.path_file}'.")
+            # Find close matches using difflib
+            close_matches = difflib.get_close_matches(file_extension.lower(), valid_extensions)
+            
+            # Display an error message with close matches
+            print(f"Error: Invalid file type for '{self.path}'. "
+                  f"Supported types are JSON, XML, and CSV. Close matches: {', '.join(close_matches)}")
+            
             return False
 
         return True
 
     def read_config(self):
+        """
+        Reads the configuration data from the file and initializes attributes.
+
+        Returns:
+            dict: The configuration data.
+        """
         if not self.is_valid_config():
             return "Configuration not valid"  # If configuration is not valid return a message
 
-        config_data = {}
-
-        if self.data_type == 'JSON':
-            with open(self.path_file, 'r') as json_file:
+        # Read the configuration from the existing file
+        try:
+            with open(self.path, 'r') as json_file:
                 config_data = json.load(json_file)
 
-        elif self.data_type == 'XML':
-            tree = ET.parse(self.path_file)
-            root = tree.getroot()
-            config_data['entity_collection'] = self.entity_collection
-            config_data['base_fields'] = [field.text for field in root.findall('.//base_fields/field')]
-            config_data['computable_fields'] = [field.text for field in root.findall('.//computable_fields/field')]
+            # Initialize attributes based on the configuration data
+            self.path = config_data.get('path', '')
+            self.data_type = config_data.get('data_type', '')
+            self.entity_collection = config_data.get('entity_collection', '')
+            self.base_field = config_data.get('base_field', '')
+            self.computable_fields = config_data.get('computable_fields', [])
 
-        elif self.data_type == 'CSV':
-            with open(self.path_file, 'r') as csv_file:
-                csv_reader = csv.DictReader(csv_file)
-                config_data['entity_collection'] = self.entity_collection
-                config_data['base_fields'] = csv_reader.fieldnames
-                config_data['computable_fields'] = [field for field in config_data['base_fields'] if field not in self.base_fields]
+            return config_data
+        
+        # Prints an error message indicating a failure to read the configuration from the specified file,
+        # including the specific exception details, and returns None to signal an unsuccessful read operation
+        except Exception as e:
+            print(f"Error reading configuration from '{self.path}': {e}")
+            return None
 
-        return config_data
+    def get(self, property_name):
+        """
+        Retrieves the value of a property.
+
+        Parameters:
+            property_name (str): The name of the property to retrieve.
+
+        Returns:
+            Any: The value of the specified property.
+        """
+        return getattr(self, property_name, None)
